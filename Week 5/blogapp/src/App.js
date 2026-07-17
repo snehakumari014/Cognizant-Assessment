377dabac76c76a78ee089fd5736ffe4bfd44c360
snehakumari014 <snehakumaripandey14@gmail.com>
2026-07-17 22:02:41 +0530

    Added Week 5 React Lifecycle Hands-On

---
+++ Week 5/blogapp/src/App.js
@@ -0,0 +1,11 @@
+import Posts from "./Components/Posts";
+
+function App() {
+  return (
+    <div>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 5/blogapp/src/Components/Posts.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ posts: data.slice(0, 10) });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error) {
+    alert("Error: " + error);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+
+        {this.state.posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            body={post.body}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
