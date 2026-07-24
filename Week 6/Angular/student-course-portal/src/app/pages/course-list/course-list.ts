90a7722ae0a02508f8e6c5a04efa0cc5bca095b7
snehakumari014 <snehakumaripandey14@gmail.com>
2026-07-24 21:51:17 +0530

    Completed Angular Hands-On 2

--- Week 6/Angular/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Week 6/Angular/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,9 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+
+  courses = [
+    {
+      id: 1,
+      name: 'Angular Fundamentals',
+      duration: '6 Weeks',
+      instructor: 'John'
+    },
+    {
+      id: 2,
+      name: 'TypeScript Basics',
+      duration: '4 Weeks',
+      instructor: 'David'
+    },
+    {
+      id: 3,
+      name: 'Web Development',
+      duration: '8 Weeks',
+      instructor: 'Smith'
+    },
+    {
+      id: 4,
+      name: 'Java Programming',
+      duration: '10 Weeks',
+      instructor: 'James'
+    }
+  ];
+  selectedCourse = '';
+  searchText = '';
+  showCourses = true;
+
+  toggleCourses() {
+  this.showCourses = !this.showCourses;
+}
+
+enroll(courseName: string) {
+  this.selectedCourse = courseName;
+  alert('You enrolled in ' + courseName);
+}
+
+}
